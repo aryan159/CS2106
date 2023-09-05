e4dc228e1083a8271a95b79a21d028dd5a7472b7
Aryan Jain <33245117+aryan159@users.noreply.github.com>
2023-09-05 11:52:25 +0800

    Add files via upload

---
+++ Lab1Programs/part1/queue.h
@@ -0,0 +1,16 @@
+
+// Maximum number of elements in a queue
+//
+#define MAX_Q_SIZE 10
+
+// Place function prototypes here.
+
+void enq(double);
+double deq();
+
+void sum(double);
+void prod(double);
+void clear_sum();
+void clear_prod();
+double reduce();
+double flex_reduce(void (*clear)(), void (*op)(double));
