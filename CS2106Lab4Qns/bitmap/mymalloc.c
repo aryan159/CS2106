1e418f287cbe4843db4a37694e6ab2d6e2cfe502
Aryan Jain <aryanjain987@gmail.com>
2023-11-07 11:08:21 +0800

    Lab4 progress

--- CS2106Lab4Qns/bitmap/mymalloc.c
+++ CS2106Lab4Qns/bitmap/mymalloc.c
@@ -1,29 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "mymalloc.h"
+#include "bitmap.h"
+#include "llist.h"
 
 char _heap[MEMSIZE] = {0};
+TNode *root = NULL;
 
 // Do not change this. Used by the test harness.
 // You may however use this function in your code if necessary.
-long get_index(void *ptr) {
-    if(ptr == NULL)
+long get_index(void *ptr)
+{
+    if (ptr == NULL)
         return -1;
     else
-        return (long) ((char *) ptr - &_heap[0]);
+        return (long)((char *)ptr - &_heap[0]);
 }
 
-void print_memlist() {
+void print_memlist()
+{
     // Implement this to call print_map from bitmap.c
 }
 
 // Allocates size bytes of memory and returns a pointer
 // to the first byte.
-void *mymalloc(size_t size) {
-    return NULL;
+void *mymalloc(size_t size)
+{
+
+    // Find index in bitmap
+    long index = search_map(_heap, MEMSIZE / 8, size);
+    if (index == -1)
+    {
+        return NULL;
+    }
+
+    // Create and insert node into linked list
+    TData *tData = malloc(sizeof(TData));
+    tData->len = size;
+    TNode *tNode = make_node(index, tData);
+    insert_node(&root, tNode, ASCENDING);
+
+    // Allocate the bitmap
+    allocate_map(_heap, index, size);
+
+    // Return bitmap pointer?
+    return &_heap[index];
 }
 
 // Frees memory pointer to by ptr.
-void myfree(void *ptr) {
-}
+void myfree(void *ptr)
+{
 
+    long index = get_index(ptr);
+    if (index == -1)
+    {
+        return;
+    }
+
+    // Get length from linked list
+    TNode *node = find_node(root, index);
+    if (node == NULL)
+    {
+        return;
+    }
+    long length = node->pdata->len;
+
+    // Reset the bitmap
+    free_map(_heap, index, length);
+
+    // Delete node from linked list
+    free(node->pdata);
+    delete_node(&root, node);
+}
